4753e83d3de55ddbd89aa3f386d97bf6e9336a0e
Kalana Lakshan <klacc2022@gmail.com>
2025-03-08 20:42:33 +0530

    Create playerController.js

---
+++ backend/controllers/playerController.js
@@ -0,0 +1,10 @@
+const Player = require('../models/Player');
+
+exports.getAllPlayers = async (req, res) => {
+    try {
+        const players = await Player.find();
+        res.json(players);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
